65a657932f93ce73e3f73a0412b873547d243e6a
bimuyu <bimuyu@helijia.com>
2020-04-15 11:30:40 +0800

    add _app.js

---
+++ pages/_app.js
@@ -0,0 +1,7 @@
+import 'antd/dist/antd.css'
+import '../public/style.css'
+
+// This default export is required in a new `pages/_app.js` file.
+export default function App({ Component, pageProps }) {
+  return <Component {...pageProps} />
+}
